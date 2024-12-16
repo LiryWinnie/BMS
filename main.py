e5901e8e9f73f6e22ae551e92b3dbb4f4d49354d
winniePeng <18213452674@163.com>
2024-12-16 20:07:32 +0800

    提交跳转修改

--- main.py
+++ main.py
@@ -64,57 +64,6 @@ class LoginManager(QMainWindow, Ui_MainWindow):
             # 注册成功跳转到登录页面
             self.click_login()
 
-class BMSManager(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super(BMSManager,self).__init__()
-        self.setupUi(self)
-        self.pushButton_6.clicked.connect(self.click_add)
-        self.pushButton_7.clicked.connect(self.click_del)
-        self.pushButton_8.clicked.connect(self.click_search)
-        self.pushButton_9.clicked.connect(self.click_modify)
-        self.pushButton_4.clicked.connect(self.add_book)
-    
-    def click_add(self):
-        """ 
-            跳转添加图书页面
-        """
-        self.stackedWidget.setCurrentIndex(0)
- 
-    def click_del(self):
-        """ 
-            跳转删除图书页面
-        """
-        self.stackedWidget.setCurrentIndex(1)
-
-    def click_search(self):
-        """ 
-            跳转查找图书页面
-        """
-        self.stackedWidget.setCurrentIndex(2)
- 
-    def click_modify(self):
-        """ 
-            跳转修改信息页面
-        """
-        self.stackedWidget.setCurrentIndex(3)
-    
-    def add_book(self):
-        """ 
-            添加图书操作
-        """
-        bookname = self.lineEdit_2.text()
-        author = self.lineEdit_6.text()
-        num = self.lineEdit_7.text()
-        address = self.lineEdit_8.text()
-
-        new_book = Book(bookname,author,num,address)
-        if new_book.addBook():
-            QMessageBox.information(None,"信息","添加成功！",QMessageBox.Close)
-        else:
-            QMessageBox.warning(None,"错误","书名已存在！",QMessageBox.Close)
-    
-
-    
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
